153b942f95064eb48e4367e2af845ce2d6f15b74
joaopaulu <jptick@gmail.com>
2023-05-23 17:35:28 -0300

    Alterando nomes dos testes unitários

--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,19 +1,31 @@
 package com.algaworks.junit.utilidade;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.*;
 
+
+@DisplayName("Testes no utilitário de saudação")
 class SaudacaoUtilTest {
 
     @Test
+    @DisplayName("Deve saudar com bom dia")
     public void saudarComBomDia() {
+        //Arrange
         int horaValida = 9;
+
+        //Act
         String saudacao = saudar(horaValida);
+
+        //Assert
         assertEquals("Bom dia", saudacao);
     }
 
     @Test
+    @DisplayName("Deve saudar com bom dia às 5 horas")
     public void saudarComBomDiaAPartir5h() {
         int horaValida = 5;
         String saudacao = saudar(horaValida);
@@ -43,14 +55,17 @@ class SaudacaoUtilTest {
 
     @Test
     public void deveLancarException() {
-        IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> saudar(-10));
-        assertEquals("Hora inválida", illegalArgumentException.getMessage());
+        int horaInvalida = -10;
+        Executable chamadaInvalidaDeMetodo = () -> saudar(horaInvalida);
+        IllegalArgumentException e = assertThrows(IllegalArgumentException.class, chamadaInvalidaDeMetodo);
+        assertEquals("Hora inválida", e.getMessage());
     }
 
     @Test
     public void naoDeveLancarException() {
-        assertDoesNotThrow(()-> saudar(0));
+        int horaValida = 0;
+        Executable chamadaValidaDeMetodo = () -> saudar(horaValida);
+        assertDoesNotThrow(chamadaValidaDeMetodo);
     }
 
 }
