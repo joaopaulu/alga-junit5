ed5778d7343754777a6a06b5def5d381d96b3df8
joaopaulu <jptick@gmail.com>
2023-05-23 17:14:24 -0300

    Finalizando Módulo 2 - Explorando o JUnit

--- README.md
+++ README.md
@@ -23,3 +23,20 @@
 1.13. Baixando o projeto inicial do Github
 1.14. Comandos básicos do Maven
 1.15. Apresentação do projeto do curso
+
+### 2. Explorando o JUnit
+
+2.1. Introdução ao JUnit
+2.2. Escrevendo o seu primeiro teste unitário
+2.3. Explorando as asserções
+2.4. Asserções de Exceptions
+2.5. Asserções em listas
+2.6. Asserções de Timeout
+2.7. Asserções agrupadas com AssertAll
+2.8. Executando do teste com Debug
+2.9. Desabilitando testes unitários
+2.10. Ignorando execução dos testes condicionalmente com Assumptions
+2.11. Executando testes via Maven
+2.12. Desafio - Escrevendo testes faltantes do SaudacaoUtil
+2.13. Refatorando classe SaudacaoUtil
+2.14. Desafio - Implementando conta bancária com testes unitários
--- src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
+++ src/test/java/com/algaworks/junit/utilidade/SaudacaoUtilTest.java
@@ -1,28 +1,56 @@
 package com.algaworks.junit.utilidade;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static com.algaworks.junit.utilidade.SaudacaoUtil.saudar;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SaudacaoUtilTest {
 
     @Test
-    public void saudar() {
-        String saudacao = SaudacaoUtil.saudar(9);
-        assertEquals("Bom dia", saudacao, "Saudação incorreta!");
+    public void saudarComBomDia() {
+        int horaValida = 9;
+        String saudacao = saudar(horaValida);
+        assertEquals("Bom dia", saudacao);
+    }
+
+    @Test
+    public void saudarComBomDiaAPartir5h() {
+        int horaValida = 5;
+        String saudacao = saudar(horaValida);
+        assertEquals("Bom dia", saudacao);
+    }
+
+    @Test
+    public void saudarComBoaTarde() {
+        int horaValida = 15;
+        String saudacao = saudar(horaValida);
+        assertEquals("Boa tarde", saudacao);
+    }
+
+    @Test
+    public void saudarComBoaNoite() {
+        int horaValida = 22;
+        String saudacao = saudar(horaValida);
+        assertEquals("Boa noite", saudacao);
+    }
+
+    @Test
+    public void saudarComBoaNoiteAs4h() {
+        int horaValida = 4;
+        String saudacao = saudar(horaValida);
+        assertEquals("Boa noite", saudacao);
     }
 
     @Test
     public void deveLancarException() {
         IllegalArgumentException illegalArgumentException = assertThrows(IllegalArgumentException.class,
-                () -> SaudacaoUtil.saudar(-10));
+                () -> saudar(-10));
         assertEquals("Hora inválida", illegalArgumentException.getMessage());
     }
 
     @Test
     public void naoDeveLancarException() {
-        assertDoesNotThrow(()-> SaudacaoUtil.saudar(0));
+        assertDoesNotThrow(()-> saudar(0));
     }
-
 }
