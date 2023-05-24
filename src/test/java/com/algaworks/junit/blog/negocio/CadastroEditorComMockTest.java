fd8ed6ed7ddeaba8ba57ff9e804db5eaf4425339
joaopaulu <jptick@gmail.com>
2023-05-24 19:05:56 -0300

    <dependency>

--- src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
+++ src/test/java/com/algaworks/junit/blog/negocio/CadastroEditorComMockTest.java
@@ -3,29 +3,41 @@ package com.algaworks.junit.blog.negocio;
 import com.algaworks.junit.blog.armazenamento.ArmazenamentoEditor;
 import com.algaworks.junit.blog.modelo.Editor;
 import org.junit.jupiter.api.*;
-import org.mockito.Mockito;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.*;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+@ExtendWith(MockitoExtension.class)
 public class CadastroEditorComMockTest {
 
-    CadastroEditor cadastroEditor;
     Editor editor;
+
+    @Captor
+    ArgumentCaptor<Mensagem> mensagemArgumentCaptor;
+
+    @Mock
+    ArmazenamentoEditor armazenamentoEditor;
+
+    @Mock
+    GerenciadorEnvioEmail gerenciadorEnvioEmail;
+
+    @InjectMocks
+    CadastroEditor cadastroEditor;
 
     @BeforeEach
     void init() {
         editor = new Editor(null, "Alex", "alex@email.com", BigDecimal.TEN, true);
-
-        ArmazenamentoEditor armazenamentoEditor = Mockito.mock(ArmazenamentoEditor.class);
-        Mockito.when(armazenamentoEditor.salvar(editor))
-                .thenReturn(new Editor(1L, "Alex", "alex@email.com", BigDecimal.TEN, true));
-
-        GerenciadorEnvioEmail gerenciadorEnvioEmail = Mockito.mock(GerenciadorEnvioEmail.class);
-
-        cadastroEditor = new CadastroEditor(armazenamentoEditor, gerenciadorEnvioEmail);
+        Mockito.when(armazenamentoEditor.salvar(Mockito.any(Editor.class)))
+                .thenAnswer(invocacao -> {
+                    Editor editorPassado = invocacao.getArgument(0, Editor.class);
+                    editorPassado.setId(1L);
+                    return editorPassado;
+                });
     }
 
     @Test
@@ -34,4 +46,30 @@ public class CadastroEditorComMockTest {
         assertEquals(1L, editoSalvo.getId());
     }
 
+    @Test
+    void Dado_um_editor_valido_Quando_criar_Entao_deve_chamar_metodo_salvar_do_armazenamento() {
+        cadastroEditor.criar(editor);
+        Mockito.verify(armazenamentoEditor, Mockito.times(1))
+                .salvar(Mockito.eq(editor));
+    }
+
+    @Test
+    void Dado_um_editor_valido_Quando_criar_e_lancar_exception_ao_salvar_Entao_nao_deve_enviar_email() {
+        Mockito.when(armazenamentoEditor.salvar(editor)).thenThrow(new RuntimeException());
+        assertAll("Não deve enviar e-mail, quando lançar Exception do armazenamento",
+                ()->assertThrows(RuntimeException.class, ()-> cadastroEditor.criar(editor)),
+                ()->Mockito.verify(gerenciadorEnvioEmail, Mockito.never()).enviarEmail(Mockito.any())
+        );
+    }
+
+    @Test
+    void Dado_um_editor_valido_Quando_cadastrar_Entao_deve_enviar_email_com_destino_ao_editor() {
+        Editor editoSalvo = cadastroEditor.criar(editor);
+
+        Mockito.verify(gerenciadorEnvioEmail).enviarEmail(mensagemArgumentCaptor.capture());
+
+        Mensagem mensagem = mensagemArgumentCaptor.getValue();
+
+        assertEquals(editoSalvo.getEmail(), mensagem.getDestinatario());
+    }
 }
